1da3decf7ad0df25568a220016425b47ea098f8d
Ali Mohammadian <alim2@vt.edu>
2019-07-19 20:43:51 -0400

    Initial Commit

---
+++ gameList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  gameList
+//
+//  Created by Ali Mohammadian on 7/19/19.
+//  Copyright © 2019 InstaDate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
